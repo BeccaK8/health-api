2e2d3a539ec188dea944309a2e94fdb635b4a3cd
BeccaK8 <150313386+BeccaK8@users.noreply.github.com>
2024-02-09 15:27:22 -0600

    added show route for health date

--- app/routes/health_date_routes.js
+++ app/routes/health_date_routes.js
@@ -14,13 +14,23 @@ const router = express.Router()
 // INDEX of MY health dates
 // GET /healthDates
 router.get('/dates', requireToken, (req, res, next) => {
-	HealthDate.find().sort( { date: 1 })
-		.then((dates) => {
-			return dates.map((d) => d.toObject())
+	HealthDate.find({ owner: req.user.id }).sort( { dateString: 1 })
+		.then((healthDates) => {
+			return healthDates.map((d) => d.toObject())
 		})
 		// respond with status 200 and JSON of the examples
-		.then((dates) => res.status(200).json({ dates: dates }))
+		.then((healthDates) => res.status(200).json({ healthDates: healthDates }))
 		// if an error occurs, pass it to the handler
+		.catch(next)
+})
+
+// SHOW
+// GET /healthDates/5a7db6c74d55bc51bdf39793
+router.get('/dates/:id', requireToken, (req, res, next) => {
+
+	HealthDate.findById(req.params.id)
+		.then(handle404)
+		.then((healthDate) => res.status(200).json({ healthDate: healthDate.toObject() }))
 		.catch(next)
 })
 
